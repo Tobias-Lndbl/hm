fa4790a08060d753d3e5abed90b46ccc2ec148f6
Tobias-Lndbl <phoenixt20000@gmail.com>
2025-07-11 15:15:40 +0200

    forgot to add home.nix

--- home.nix
+++ home.nix
@@ -33,39 +33,39 @@
     };
   };
 
-  /*nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "vscode"
-    "google-chrome"
-    "discord"
-    "spotify"
-  ];
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    vscode
-    google-chrome
-    git
-    whatsapp-for-linux
-    discord
-    spotify
-    zulu23
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];*/
+# #/* nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+#    "vscode"
+#    "google-chrome"
+#    "discord"
+#    "spotify"
+#  ];
+#  # The home.packages option allows you to install Nix packages into your
+#  # environment.
+#  home.packages = with pkgs; [
+#    vscode
+#    google-chrome
+#    git
+#    whatsapp-for-linux
+#    discord
+#    spotify
+#    zulu23
+#    # # Adds the 'hello' command to your environment. It prints a friendly
+#    # # "Hello, world!" when run.
+#    # pkgs.hello
+#
+#    # # It is sometimes useful to fine-tune packages, for example, by applying
+#    # # overrides. You can do that directly here, just don't forget the
+#    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+#    # # fonts?
+#    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+#
+#    # # You can also create simple shell scripts directly inside your
+#    # # configuration. For example, this adds a command 'my-hello' to your
+#    # # environment:
+#    # (pkgs.writeShellScriptBin "my-hello" ''
+#    #   echo "Hello, ${config.home.username}!"
+#    # '')
+#  ];*/
 
   programs.bash = {
   enable = true;
