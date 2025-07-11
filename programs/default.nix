2f9a0a8c9a21bb12caa7a7e8fc78b398026ddbdb
Tobias-Lndbl <phoenixt20000@gmail.com>
2025-07-11 13:50:08 +0200

    changed browser from google-chrome to chromium due to google chrome not being able to be opened via the console

--- programs/default.nix
+++ programs/default.nix
@@ -3,10 +3,11 @@
   imports = [
     ./libresprite.nix
     ./discord.nix
+    ./chromium.nix
     ./element.nix 
     ./git.nix
     ./gnome-tweaks.nix
-    ./google-chrome.nix
+#    ./google-chrome.nix
     ./spotify.nix
     ./unity.nix
     ./vscode.nix
--- programs/google-chrome.nix
+++ programs/google-chrome.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{config, pkgs, lib, ... }:
 
 {
   home.packages = with pkgs; [
