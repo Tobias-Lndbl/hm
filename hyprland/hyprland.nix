422f470d92f5e32b82ff4c3bebf14168195bcd03
Tobias-Lndbl <phoenixt20000@gmail.com>
2025-07-15 18:08:38 +0200

    added some more shortcuts

--- hyprland/hyprland.nix
+++ hyprland/hyprland.nix
@@ -1,8 +1,8 @@
 {pkgs, config, ...}:
 let 
-  switch_workspace = builtins.map (ws: "SUPER, ${ws}, workspace, ${ws}") config.workspaces;
+#  switch_workspace = builtins.map (ws: "SUPER, ${ws}, workspace, ${ws}") config.workspaces;
 
-  move_workspace = builtins.map (ws: "SUPER_SHIFT, ${ws}, movetoworkspace, ${ws}") config.workspaces;
+#  move_workspace = builtins.map (ws: "SUPER_SHIFT, ${ws}, movetoworkspace, ${ws}") config.workspaces;
 
 in
 {
