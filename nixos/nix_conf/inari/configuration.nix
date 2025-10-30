998f5148ed9d179b017d9c9a7c9800199b36a549
Tobias-Lndbl <phoenixt20000@gmail.com>
2025-10-30 12:44:26 +0100

    done some things, including making xounalpp intransparent

--- nixos/nix_conf/inari/configuration.nix
+++ nixos/nix_conf/inari/configuration.nix
@@ -16,6 +16,8 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  services.logind.powerKey = "suspend";
+
   # Enable networking
   networking.networkmanager.enable = true;
 
