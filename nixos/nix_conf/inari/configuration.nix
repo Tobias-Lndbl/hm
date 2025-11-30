205e39f89e37e64fa6b51e2f79c6f2255c34470d
Tobias-Lndbl <phoenixt20000@gmail.com>
2025-11-30 14:20:07 +0100

    added digital and installed open jdk

--- nixos/nix_conf/inari/configuration.nix
+++ nixos/nix_conf/inari/configuration.nix
@@ -57,6 +57,7 @@
   environment.systemPackages = with pkgs; [
     brightnessctl
     wirelesstools
+    jdk
   ];
 
 
--- programs/TUM/default.nix
+++ programs/TUM/default.nix
@@ -3,5 +3,6 @@
   imports = [
     ./eduvpn-client.nix
     ./qtrvsim.nix
+    ./digital.nix
   ];
 }
