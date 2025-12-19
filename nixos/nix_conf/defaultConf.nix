26274c85123ac2a7350d0c8e250626a612ed3b0b
Tobias-Lndbl <phoenixt20000@gmail.com>
2025-12-19 17:31:57 +0100

    renamed services.logind.powerKey to services.logind.settings.Login.HandlePowerKey

--- nixos/nix_conf/defaultConf.nix
+++ nixos/nix_conf/defaultConf.nix
@@ -73,7 +73,7 @@
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 
-  services.logind.powerKey = "suspend";
+  services.logind.settings.Login.HandlePowerKey = "suspend";
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
