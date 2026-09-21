a1e263f78c8c1dd01451b3085ecf6775873c93a4
Tobias-Lndbl <phoenixt20000@gmail.com>
2026-09-21 16:20:51 +0200

    inari external monitor setup

--- nixos/hyprland/inari_hyprland.nix
+++ nixos/hyprland/inari_hyprland.nix
@@ -20,6 +20,14 @@
         scale = 1.6,
       })
 
+      hl.monitor({
+        output = "DP-2",
+        -- mode = "2560x1440@59",
+        mode = "1920x1080@60.00",
+        position = "auto",
+        scale = 1,
+      })
+
       hl.on("hyprland.start", function()
         hl.exec_cmd("iio-hyprland")
       end)
